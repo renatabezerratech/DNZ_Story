530ba878a60acb6738a19de4130e797c7098a0b6
Renata Bezerra <renatabezerratecnologia@gmail.com>
2021-08-03 18:32:46 -0300

    inicio

---
+++ Site/DNZ.js
@@ -0,0 +1,4 @@
+function clicou(){
+    var nome = window.prompt('Qual é o seu nome?');
+    window.alert(`Olá ${nome}, que bom te ver por aqui! Espero que você goste da experiência de adquirir os produtos da loja. São coisas lindas e feitas com carinho para você.`);
+    }
